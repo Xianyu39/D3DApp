4fdfca66e93423eed768d607496fe90c92947f1b
Xianyu <fanghejin@qq.com>
2022-08-22 18:41:05 +0800

    整理了GameApp中的各种变量和函数

--- include/GameApp.h
+++ include/GameApp.h
@@ -1,43 +1,51 @@
 #if !defined(GAMEAPP_H)
 #define GAMEAPP_H
 
+#include <DirectXMath.h>
+#include <vector>
+#include <memory>
+
 #include "D3DApp.h"
 #include "wrl/client.h"
 #include "Vertex.h"
 #include "ConstantBuffer.h"
+#include "Transform.h"
+#include "Geometry.h"
+#include "Camera.h"
+#include "GameObject.h"
 
 class GameApp :public D3DApp {
 protected:
     template<class T>
     using ComPtr = Microsoft::WRL::ComPtr<T>;
 
-    std::wstring vertexShaderSrcPath;
-    std::wstring pixelShaderSrcPath;
     ComPtr<ID3D11VertexShader> pVertexShader;
     ComPtr<ID3D11PixelShader> pPixelShader;
     ComPtr<ID3D11InputLayout> pInputLayout;
-    ComPtr<ID3D11Buffer> pVertexBuffer;
-    ComPtr<ID3D11Buffer> pIndexBuffer;
-    ComPtr<ID3D11Buffer> pVConstantBuffer;
-    ComPtr<ID3D11Buffer> pPConstantBuffer;
+    std::vector<ComPtr<ID3D11Buffer>> pConstantBuffers;
 
-    VConstantBuffer vcbuffer;
-    PSConstantBuffer pcbuffer;
-    UINT indexNum;
+    CBChangesEveryFrame cbFrame;
+    CBChangesEveryDrawing cbDrawing;
+    CBChangesOnResize cbResize;
+    CBChangesRarely cbRare;
+
+    std::shared_ptr<Camera> pCamera;
+    ComPtr<ID3D11SamplerState> pSamplerState;
+
+    GameObject woodBox;
+    GameObject floor;
+    std::vector<GameObject> walls;
 
 public:
-    
     GameApp(HINSTANCE hInstance)
-        :D3DApp(hInstance, L"DX11", 800, 450) {}
+        :D3DApp(hInstance, L"DX11", 800, 450), pConstantBuffers(4, nullptr), walls(4) {}
     GameApp(HINSTANCE hInstance, const WCHAR* caption, UINT width, UINT height)
-        :D3DApp(hInstance, caption, width, height){}
+        :D3DApp(hInstance, caption, width, height), pConstantBuffers(4, nullptr), walls(4) {}
     ~GameApp() {}
     void UpdateScene(float dt)override;
     void DrawScene()override;
     bool InitEffect();
     bool InitResource();
     bool Init()override;
-    void setVertexShaderSrc(const std::wstring& fullname) { vertexShaderSrcPath = fullname; }
-    void setPixelShaderSrc(const std::wstring& fullname) { pixelShaderSrcPath = fullname; }
 };
 #endif // GAMEAPP_H
