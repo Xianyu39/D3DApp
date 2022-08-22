cdbe96fbdbdaffb90ff213f7cb79d6abac034fa0
Xianyu <fanghejin@qq.com>
2022-08-22 18:41:43 +0800

    新的着色器

--- HLSL/ps.hlsl
+++ HLSL/ps.hlsl
@@ -1,56 +1,5 @@
 #include "Vertex.hlsli"
-
-// 方向光
-struct DirectionalLight
-{
-    float4 ambient;
-    float4 diffuse;
-    float4 specular;
-    float3 direction;
-    float pad;
-};
-
-// 点光
-struct PointLight
-{
-    float4 ambient;
-    float4 diffuse;
-    float4 specular;
-
-    float3 position;
-    float range;
-
-    float3 att;
-    float pad;
-};
-
-// 聚光灯
-struct SpotLight
-{
-    float4 ambient;
-    float4 diffuse;
-    float4 specular;
-
-    float3 position;
-    float range;
-
-    float3 direction;
-    float Spot;
-
-    float3 att;
-    float pad;
-};
-
-// 物体表面材质
-struct Material
-{
-    float4 ambient;
-    float4 diffuse;
-    float4 specular; // w = SpecPower
-    float4 reflect;
-};
-
-
+#include "Constants.hlsli"
 
 void ComputeDirectionalLight(Material mat, DirectionalLight L,
     float3 normal, float3 toEye,
@@ -179,22 +128,7 @@ void ComputeSpotLight(Material mat, SpotLight L, float3 pos, float3 normal, floa
     spec *= att;
 }
 
-cbuffer PSConstantBuffer : register(b1)
-{
-    DirectionalLight g_DirLight[10];
-    PointLight g_PointLight[10];
-    SpotLight g_SpotLight[10];
-    Material g_Material;
-    int g_NumDirLight;
-    int g_NumPointLight;
-    int g_NumSpotLight;
-    float g_Pad1;
-
-    float3 g_EyePosW;
-    float g_Pad2;
-}
 Texture2D gTex:register(t0);
-Texture2D gTexMask:register(t1);
 
 SamplerState gSamLinear:register(s0);
 
@@ -202,29 +136,29 @@ float4 main(VertexOut v):SV_Target{
     float4 ambient, diffuse, specular, A,D,S;
     v.normalW = normalize(v.normalW);
     int i=0;
-    for(i=0; i<g_NumDirLight; ++i){
-        ComputeDirectionalLight(g_Material, g_DirLight[i], v.normalW, normalize(g_EyePosW-v.posW), A, D,S);
+    for(i=0; i<numDirLight; ++i){
+        ComputeDirectionalLight(material, dirLight[i], v.normalW, normalize(eyePos.xyz -v.posW), A, D,S);
         ambient+=A;
         diffuse+=D;
         specular+=S;
     }
-    for(i=0; i<g_NumPointLight; ++i){
-        ComputePointLight(g_Material, g_PointLight[i],v.posW,  v.normalW, normalize(g_EyePosW-v.posW), A, D,S);
+    for(i=0; i<numPointLight; ++i){
+        ComputePointLight(material, pointLight[i],v.posW,  v.normalW, normalize(eyePos.xyz-v.posW), A, D,S);
         ambient+=A;
         diffuse+=D;
         specular+=S;
     }
-    for(i=0; i<g_NumSpotLight; ++i){
-        ComputeSpotLight(g_Material, g_SpotLight[i], v.posW ,v.normalW, normalize(g_EyePosW-v.posW), A, D,S);
+    for(i=0; i<numSpotLight; ++i){
+        ComputeSpotLight(material, spotLight[i], v.posW ,v.normalW, normalize(eyePos.xyz-v.posW), A, D,S);
         ambient+=A;
         diffuse+=D;
         specular+=S;
     }
 
     float4 texColor = gTex.Sample(gSamLinear, v.tex);
-    texColor *= gTexMask.Sample(gSamLinear, v.tex);
+    // texColor *= gTexMask.Sample(gSamLinear, v.tex);
 
     float4 color = texColor*(diffuse+ambient)+specular;
-    color.a = texColor.a*g_Material.diffuse.a;
+    color.a = texColor.a*material.diffuse.a;
     return color;
 }
--- HLSL/vs.hlsl
+++ HLSL/vs.hlsl
@@ -1,12 +1,5 @@
 #include "Vertex.hlsli"
-
-cbuffer VCbuffer:register(b0){
-    float4x4 world;
-    float4x4 view;
-    float4x4 proj;
-    float4x4 worldInv;
-    float4x4 texTrans;
-};
+#include "Constants.hlsli"
 
 VertexOut main(VertexIn v){
     VertexOut ans;
@@ -14,8 +7,7 @@ VertexOut main(VertexIn v){
     float4 posW = mul(float4(v.pos, 1.f), world);
     ans.posH = mul(posW, viewproj);
     ans.posW = posW.xyz;
-    ans.tex = mul(float4(v.tex-0.5, 0, 1), texTrans).xy;
-    ans.tex += 0.5;
+    ans.tex = v.tex;
     ans.normalW = mul(v.normal, (float3x3)worldInv);
     ans.color = float4(normalize(v.normal), 1.0f);
     return ans;
